285109d6044b9205a488306d5e5f0b5510e16927
Shri10-varshni <146243128+Shri10-varshni@users.noreply.github.com>
2023-12-03 20:53:01 +0530

    Update models.py
    Another table containing user data was added and the relationship between the user table and the tasks table was established

--- Round_2_Delta/models.py
+++ Round_2_Delta/models.py
@@ -1,17 +1,36 @@
-# Taables(models) required for the API
+# This files contains the structure of all the tables used for storing data in this API
 
-from sqlalchemy import Column, Integer, String, DateTime, Date
+from database import Base 
+from sqlalchemy import Column, Integer, String, DateTime, Date, Boolean, Enum, ForeignKey
+from sqlalchemy.orm import relationship
 
-from database import Base
+# Structure of the Table to store user data
+class Userdb(Base):
 
-# Structure of the table - Task
-class Task(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_name = Column(String, unique=True, index=True)
+    full_name = Column(String, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=False)
+
+    # Establishing a one-to-many relationship with TaskDb
+    tasks = relationship("Taskdb", back_populates="user")
+
+# Structure of the table to store Task data
+class Taskdb(Base):
+
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, index=True)
+    id = Column(Integer, auto_increment=True, primary_key=True, index=True)
+    name = Column(String, index=True, default='Untitled Task')
+    description = Column(String, index=True, default=None)
     deadline = Column(Date, index=True, default=None)
     reminder = Column(DateTime, index=True, default=None)
-    status = Column(String, index=True, default=None)
-    priority = Column(String, index=True, default=None)
+    status = Column(Enum('Pending', 'Completed'), index=True, default='Pending')
+    priority = Column(Enum('High', 'Medium', 'Low'), index=True, default='Medium')
+
+    # Establishing a many-to-one relationship with UserDb
+    user_id = Column(Integer, ForeignKey(Userdb.user_id))
+    user = relationship("Userdb", back_populates="tasks")
