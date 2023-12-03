32cfe627b022a816ce5c7f73c577cca30083ddb2
Shri10-varshni <146243128+Shri10-varshni@users.noreply.github.com>
2023-12-03 20:54:37 +0530

    Update schemas.py
    Schemas for user and token related data was added

--- Round_2_Delta/schemas.py
+++ Round_2_Delta/schemas.py
@@ -1,11 +1,46 @@
-# Schemas required for various task functionalities
+# This file contains all the schemas required for data validation
 
 from pydantic import BaseModel
 from typing import Optional
+
 from datetime import date, datetime
 
+# Schema for token data 
+      
+class TokenData(BaseModel):
+    user_name: str | None = None
+
+    class Config:
+        orm_mode = True
+
+
+# Schemas for user data
+    
+class User(BaseModel):
+    user_name: str
+    full_name: str | None = None
+    is_active: bool | None = None
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    user_name: str
+    full_name: str | None = None
+    password: str
+
+    class Config:
+        orm_mode = True
+        
+class UserInDB(User):
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
+
+# Schemas for task data
+
 class TaskBase(BaseModel):
-    id: int
     name: str
     description: Optional[str] = None
     deadline: Optional[date] = None
@@ -14,17 +49,11 @@ class TaskBase(BaseModel):
     priority: Optional[str] = None
 
     class Config:
-        orm_mode = True
-
-
-class TaskCreate(TaskBase):
-    pass
-
-class TaskUpdate(TaskBase):
-    pass
+        orm_mode = True        
 
 class Task(TaskBase):
     id: int
+    user_id: int
 
     class Config:
-        orm_mode = True
+        orm_mode = True        
