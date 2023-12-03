4cf6708dbace50e476fcfd6724bc83776f2af519
Shri10-varshni <146243128+Shri10-varshni@users.noreply.github.com>
2023-12-03 20:51:15 +0530

    Update database.py

--- Round_2_Delta/database.py
+++ Round_2_Delta/database.py
@@ -1,12 +1,14 @@
-# Establishing connection with the database
+# This file establishes database connection 
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
+# This stores an instance to the sqlite database (task_api.db)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./task_api.db"
 
 engine = create_engine(
+
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
